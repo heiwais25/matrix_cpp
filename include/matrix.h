1a8dab84e6118e066ae347e2c5b3819e58f135f6
jonghyun <heiwais25@gmail.com>
2018-01-15 10:24:38 +0900

    Matrix class succeed

--- include/matrix.h
+++ include/matrix.h
@@ -1,6 +1,81 @@
-#include <iostream>
+#ifndef MATRIX_
+#define MATRIX_
+
 #include <valarray>
-#include "matrix.h"
+#include <iostream>
+#include <type_traits>
+#include <typeinfo>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+using std::cerr;
+using std::endl;
+using std::cout;
+
+struct MatrixSizeException{};
+struct OutOfIndexException{};
+template <class element_type>
+class Matrix{
+	public:
+		Matrix(void):
+			m_height(1), m_stride(1), m_storage(1)
+			{};
+
+		Matrix(size_t height, size_t width):
+			m_height(height), m_stride(width), m_storage(width * height)
+			{};
+
+		element_type & operator()(size_t row, size_t column);
+		element_type & operator[](size_t num);
+		bool operator==(Matrix & m); // Return true if all element is same
+		Matrix operator*(Matrix & m); // Calculate elementwise product
+		Matrix operator+(Matrix & m);	
+
+		template <typename T>
+		Matrix operator*(T val);
+
+		// Init matrix by vector
+		template <typename T>
+		void operator=(const std::vector<T> & v);
+
+		// Return norm of the matrix
+		double norm(void);
+
+		// Return max / min value of this matrix
+		element_type max(void);
+		element_type min(void);		
+
+		// Return total size of m_storage(m_height x m_stride)
+		size_t size();
+
+		// Return reshaped matrix
+		Matrix reshape(size_t new_height, size_t new_stride);
+
+
+		// Calculate dot product
+		Matrix dot(Matrix &m);
+
+		// Print all element with its dimension
+		void print(void);
+		
+		// Return dimension matrix
+		Matrix dims(void);
+		
+		// Sum direction (1 : sum each row, 2 : sum each cloumn)
+		Matrix sum(char direction);
+		
+
+	private:
+		std::valarray<element_type> m_storage;
+		size_t m_stride;
+		size_t m_height;
+};
+
+/* =======================================================================
+
+	Operator overloading
+
+======================================================================= */
 
 template <class element_type>
 element_type & Matrix<element_type>::operator()(size_t row, size_t column){
@@ -86,7 +161,11 @@ Matrix<element_type> Matrix<element_type>::operator*(T val){
 	return *this;
 }
 
+/* =======================================================================
 
+	Method overloading
+
+======================================================================= */
 template <class element_type>
 Matrix<element_type> Matrix<element_type>::reshape(size_t new_height, size_t new_stride){
 	if(new_height * new_stride != m_storage.size())
@@ -199,3 +278,6 @@ template <class element_type>
 size_t Matrix<element_type>::size(){
 	return m_storage.size();
 }
+
+
+#endif 
---
+++ test/matrix_test.cpp
@@ -0,0 +1,79 @@
+#include "matrix.h"
+
+int main(void){
+	Matrix<double> matrix(5,2);
+	Matrix<double> matrix2(5,2);
+	cout << (matrix == matrix2) << endl;
+	matrix(1,1) = 5;
+	matrix2(4,1) = 6;
+	Matrix<double> v(5,1);	
+	v[0] = 1;
+	v[1] = 2;
+	v[2] = 3;
+	v[3] = 4;
+	v[4] = 4;
+	Matrix<double> m = matrix + matrix2;
+	m.print();
+	double t = 2;
+	m = m*t;
+	m.print();
+	m = m + v;
+	m.print();
+
+	// Dot example
+	cout.setf(std::ios::left);
+	cout.fill('=');
+	cout << std::setw(50) << "=" << endl;
+	cout << "Dot product test" << endl;
+	Matrix<double> m1(1,3), m2(3,1);
+	std::vector<double> v1 = {1,2,3}, v2 = {1,2,1};
+	m1 = v1;
+	m2 = v2;
+	m1.print();
+	cout << "dot" << endl;
+	m2.print();
+	cout << "Result : " << m1.dot(m2)[0] << endl;
+
+	// Reshape example
+	cout << std::setw(50) << "=" << endl;
+	cout << "Reshape test" << endl;
+	Matrix<double> m3(3,5);
+	cout << "before" << endl;
+	m3 = std::vector<double>{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+	m3.print();
+	cout << "after" << endl;
+	m3.reshape(5,3).print();
+
+	// Norm example
+	cout << std::setw(50) << "=" << endl;
+	cout << "Norm test" << endl;
+	Matrix<double> m4(2,2);
+	m4 = std::vector<double>{1,2,3,4};
+	m4.print();
+	cout << "Norm : " << m4.norm() << endl;
+
+	// Max, min example
+	cout << std::setw(50) << "=" << endl;
+	cout << "Max/Min test" << endl;
+	Matrix<double> m5(2,2);
+	m5 = std::vector<double>{1,2,3,4};
+	m5.print();
+	cout << "Max : " << m5.max() << " Min : " << m5.min() << endl;
+
+	// Max, min example
+	cout << std::setw(50) << "=" << endl;
+	cout << "Entrywise product test" << endl;
+	Matrix<double> m6(2,2), m7(2,2);
+	m6 = std::vector<double>{1,2,3,4};
+	m7 = std::vector<double>{1,2,3,4};
+	m6.print();
+	m7.print();
+	cout << "Result" << endl;
+	(m6*m7).print();
+
+
+	return 0;
+
+	// Max example
+}
+
